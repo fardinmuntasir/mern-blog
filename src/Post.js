bfa2db43716986fdb7019f1bfa6d03ee51940dc9
Fardin Muntasir <fardinmuntasir@gmail.com>
2024-05-01 16:49:44 -0230

    test

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+            <div className="image">
+                <img src="https://techcrunch.com/wp-content/uploads/2016/08/gettyimages-475158195.jpg?w=730&crop=1" alt=""></img>
+            </div>
+            <div className="texts">
+                <h2>12 KPIS for pitch</h2>
+                <p className="info">
+                <a className="author">Fardin</a>
+                <time> 2024-03-16</time>
+                </p>
+                <p className="summary">It is critically important for the founders of a company to intimately understand the company’s key performance indicators (KPIs). Founders cannot hope to grow a company in any meaningful way without an almost obsessive focus on its KPIs.</p>  
+            </div>
+        </div>
+    )
+}
